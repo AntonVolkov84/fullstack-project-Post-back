6e504c56fac6bc7ddc987027911cde2dcec1bc0d
antvolkov84 <antvolkov84@gmail.com>
2024-03-23 21:58:21 +0200

    add registration

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
+import { validationResult } from 'express-validator';
+
+import { registerValidator } from './validations/auth.js';
+import UserModel from './models/User.js';
 
 mongoose
   .connect(
-    'mongodb+srv://antvolkov84:G8SdLeNkI72eyvsq@cluster0.am3t6sl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+    'mongodb+srv://antvolkov84:G8SdLeNkI72eyvsq@cluster0.am3t6sl.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0'
   )
   .then(() => console.log('DB ok!'))
   .catch((err) => console.log('DB Error ', err));
@@ -12,21 +17,44 @@ mongoose
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello Anton Volkov!!!!');
-});
-app.post('/auth/login', (req, res) => {
-  console.log(req.body);
-  const token = jwt.sign(
-    {
-      email: req.body.email,
-      fullName: 'Anton Volkov',
-    },
-    'secret123'
-  );
+app.post('/auth/register', registerValidator, async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
 
-  res.json({ success: true, token });
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash: hash,
+    });
+    const user = await doc.save();
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      'secret123',
+      { expiresIn: '30d' }
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({ ...userData, token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Abort registration',
+    });
+  }
 });
+
 app.listen(4444, (error) => {
   if (error) {
     console.log(error);
