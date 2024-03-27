798cc31738178acfb01972456bfb4b3b82c76b92
antvolkov84 <antvolkov84@gmail.com>
2024-03-27 13:22:56 +0200

    backend backup

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -11,6 +11,23 @@ export const getAll = async (req, res) => {
     });
   }
 };
+export const getLastTags = async (req, res) => {
+  try {
+    const posts = await PostModel.find().limit(5).exec();
+
+    const tags = posts
+      .map((obj) => obj.tags)
+      .flat()
+      .slice(0, 5);
+
+    res.json(tags);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Can not get a posts',
+    });
+  }
+};
 
 export const create = async (req, res) => {
   try {
@@ -18,7 +35,7 @@ export const create = async (req, res) => {
       title: req.body.title,
       text: req.body.text,
       imageUrl: req.body.imageUrl,
-      tags: req.body.tags,
+      tags: req.body.tags.split(','),
       user: req.userId,
     });
 
@@ -92,7 +109,7 @@ export const update = async (req, res) => {
         text: req.body.text,
         imageUrl: req.body.imageUrl,
         user: req.userId,
-        tags: req.body.tags,
+        tags: req.body.tags.split(','),
       }
     );
     res.json({
--- index.js
+++ index.js
@@ -56,6 +56,7 @@ app.post(
 app.get('/auth/me', checkAuth, UserController.getMe);
 
 app.get('/posts', PostController.getAll);
+app.get('/tags', PostController.getLastTags);
 app.get('/posts/:id', PostController.getOne);
 app.post(
   '/posts',
